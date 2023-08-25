c0bf423bbe75935ef9aeb0735dddf1c7f57892a4
juandiegogg1801 <juandiegogg1801@gmail.com>
2023-08-24 22:14:06 -0500

    Metodo jacobi terminado

--- jacobi.py
+++ jacobi.py
@@ -1,89 +1,110 @@
 """
     Este programa resuelve un sistema de ecuaciones lineales por el metodo de Jacobi
 """
-
-from builtins import float
-
-
-def X1(x2):
-    x1 = (x2 / 4) + 1 / 4
-    return x1
+import numpy as np
 
 
-def X2(x1, x3):
-    x2 = (x1 / 4) + (x3 / 4) + 1 / 4
-    return x2
+def jacobi(A, b, n, its, tol):
+
+    # Mostrar titulo tabla
+    titulo = ' k'
+    for i in range(1, n+1):
+        titulo = titulo + '   ' + f'x{i}'
+    titulo = titulo + '   error'
+    print(titulo)
+
+    # inicializar los tres vectores con cero
+    v1 = np.zeros(n)
+    v2 = np.zeros(n)
+    v3 = np.zeros(n)
+    divs = np.zeros(n)
+    error = 1
+    k = 0
+
+    # Mostrar iteracion cero tabla
+    if k == 0:
+        chain = f' {k}'
+        for i in range(n):
+            chain = chain + f'   {v1[i]}'
+        print(chain)
+
+    while error > tol:
+        # Obtener v2
+        for i in range(n):
+            suma = 0
+            for j in range(n):
+                if i != j:
+                    suma = suma + A[i][j] * v1[j]
+                else:
+                    divs[i] = A[i][j]
+            v2[i] = b[i] - suma
+
+        for i in range(n):
+            v2[i] = v2[i] / divs[i]
+
+        # Llenar v3
+        for i in range(n):
+            v3[i] = v2[i] - v1[i]
+
+        # Encontrar maximo v3
+        max1 = abs(v3[0])
+        for i in range(1, n):
+            if abs(v3[i] >= max1):
+                max1 = abs(v3[i])
+
+        # Encontrar maximo vector2
+        max2 = abs(v2[0])
+        for i in range(1, n):
+            if abs(v2[i] >= max2):
+                max2 = abs(v2[i])
+
+        # Encontrar error relativo
+        error = max1 / max2
+
+        # Mostrar valores tabla
+        k += 1
+        cadena = f' {k}'
+        for i in range(n):
+            cadena = cadena + f'   {round(v2[i], 5)}'
+        cadena = cadena + f'   {round(error, 5)}'
+        print(cadena)
+        v1 = v2.copy()
+
+    # Comprobar si el sistema converge o diverge
+    if k > its:
+        print('El sistema diverge')
+    else:
+        print('El sistema converge')
 
 
-def X3(x2, x4):
-    x3 = (x2 / 4) + (x4 / 4) + 1 / 4
-    return x3
+# Ingreso de Datos
+n = int(input('Ingrese el numero de incognitas: '))
+A = np.zeros((n, n))
+b = np.zeros(n)
 
+# Ingresar coeficientes
+num_ecuacion = 1
+for i in range(n):
+    print(f"Ingrese los valores de la ecuacion {num_ecuacion}")
+    num_coeficiente = 1
+    for j in range(n):
+        A[i][j] = float(input(f'Ingrese el valor de x{num_coeficiente}:'))
+        num_coeficiente += 1
+    b[i] = float(input(f'Ingrese el valor de b{num_ecuacion}:'))
+    num_ecuacion += 1
 
-def X4(x3):
-    x4 = (x3 / 4) + 1 / 4
-    return x4
+# Comprobar que el sistema es estrictamente dominante
+# Recorrer matriz A
+estado = True
+for i in range(n):
+    for j in range(n):
+        if (abs(A[i][i]) < abs(A[i][j])) or (abs(A[i][i]) < abs(A[j][i])):
+            estado = False
+            break
 
-
-def jacobi(n, tol):
-    vector1 = []
-    vector2 = []
-    vector3 = []
-
-    for i in range(n):
-        vector1.append(0)
-    for i in range(n):
-        vector2.append(0)
-    for i in range(n):
-        vector3.append(0)
-
-    flag = 1
-    while flag > tol:
-        max1 = 0
-        max2 = 0
-
-        x1 = vector1[0]
-        x2 = vector1[1]
-        x3 = vector1[2]
-        x4 = vector1[3]
-
-        vector2[0] = X1(x2)
-        vector2[1] = X2(x1, x3)
-        vector2[2] = X3(x2, x4)
-        vector2[3] = X4(x3)
-
-        if abs(vector3[0]) >= abs(vector3[1]) and abs(vector3[0]) >= abs(vector3[2]) and abs(vector3[0]) >= abs(
-                vector3[3]):
-            max1 = vector3[0]
-        elif abs(vector3[1]) >= abs(vector3[0]) and abs(vector3[1]) >= abs(vector3[2]) and abs(vector3[1]) >= abs(
-                vector3[3]):
-            max1 = vector3[1]
-        elif abs(vector3[2]) >= abs(vector3[0]) and abs(vector3[2]) >= abs(vector3[1]) and abs(vector3[2]) >= abs(
-                vector3[3]):
-            max1 = vector3[2]
-        else:
-            max1 = vector3[3]
-
-        if abs(vector2[0]) >= abs(vector2[1]) and abs(vector2[0]) >= abs(vector2[2]) and abs(vector2[0]) >= abs(
-                vector2[3]):
-            max2 = vector2[0]
-        elif abs(vector2[1]) >= abs(vector2[0]) and abs(vector2[1]) >= abs(vector2[2]) and abs(vector2[1]) >= abs(
-                vector2[3]):
-            max2 = vector2[1]
-        elif abs(vector2[2]) >= abs(vector2[0]) and abs(vector2[2]) >= abs(vector2[1]) and abs(vector2[2]) >= abs(
-                vector2[3]):
-            max2 = vector2[2]
-        else:
-            max2 = vector2[3]
-
-        flag = max1 / max2
-        vector1 = vector2.copy()
-
-    print('-----RESPUESTAS------')
-    for i in range(n):
-        print('x', i, ' = ', round(vector2[i], 5))
-    print('---------------------')
-
-
-n = int(input('Ingrese el numero de incognitas'))
-jacobi(n, 0.00001)
+if estado:
+    its = int(input('Ingrese el numero de iteraciones'))
+    tol = float(input('Ingrese el valor de la tolerancia'))
+    jacobi(A, b, n, its, tol)
+else:
+    print('El sistema no es estrictamente dominante')
